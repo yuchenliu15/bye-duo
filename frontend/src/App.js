02479d4ba79b997b1c1388c11bca583a8541c7b6
yuchen <yl6448@nyu.edu>
2021-02-11 21:44:11 -0800

    add generate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
+import twoFA from '2fa-utils';
 import hi_base_32 from 'hi-base32';
 
 // 3c9b7138faaab1417406f348b31d1638
@@ -10,8 +11,24 @@ const API = 'http://127.0.0.1:5000/secret'
 function App() {
   const [link, setLink] = useState('');
   const [loading, setLoading] = useState(false);
-  const onChange = (event) => {
+  const [secret, setSecret] = useState('');
+  const [passcode, setPasscode] = useState('');
+  useEffect(() => {
+    const secret = localStorage.getItem('secret');
+    setSecret(secret);
+    if(secret) {
+      const count = localStorage.getItem('count');
+      setPasscode(twoFA.generateHOTP(secret, count));
+      localStorage.setItem('count', count + 1);
+    }
+  }, []);
+  const onChange = event => {
     setLink(event.target.value);
+  }
+  const onGenerate = event => {
+    const count = localStorage.getItem('count');
+    setPasscode(twoFA.generateHOTP(secret, count));
+    localStorage.setItem('count', count + 1);
   }
   const onClick = (event) => {
     const count = localStorage.getItem('count') || 0;
@@ -23,10 +40,9 @@ function App() {
         form)
       .then(res => {
         if(res.status === 200) {
-          localStorage.setItem('secret', res.data);
-          const encode = hi_base_32.encode(res.data)
-          console.log(encode)
-          console.log(twoFA.generateHOTP(encode, 0))
+          const secret = hi_base_32.encode(res.data)
+          localStorage.setItem('secret', secret);
+          //console.log(twoFA.generateHOTP(encode, 1))
         }
         else {
           console.error('failed');
@@ -38,8 +54,17 @@ function App() {
   }
   return (
     <div className="App">
-      <input type="text" onChange={onChange} />
-      <input type="button" value="go!" onClick={onClick} />
+      { secret?
+        <div>
+        <div>{passcode}</div>
+        <input type="button" value="generate" onClick={onGenerate} />
+        </div>
+        :
+        <div>
+          <input type="text" onChange={onChange} />
+          <input type="button" value="go!" onClick={onClick} />
+        </div>
+      }
     </div>
   );
 }
